6fe65418a4bcaee06c704a5ef0b5eb9c02f8ceb6
Camilo Carromeu <camilo@carromeu.com>
2023-07-10 17:15:24 -0400

    Finalização do script de bootstrap.

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-# docker build -t releaser --no-cache .
-docker build -t releaser .
-
 docker stop releaser && docker rm releaser
 
+docker build -t releaser --no-cache .
+# docker build -t releaser .
+
 docker run --name releaser -v /Users/camilo/Projects/embrapa.io/data:/data -d releaser
+
+# docker exec -it releaser bash
